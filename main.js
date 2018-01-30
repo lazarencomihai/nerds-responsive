55a5a0e5865cfc55dcb0a41008d982f35cea6436
mihanim <mihanim10@gmail.com>
2018-01-30 12:24:21 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+ var navMain = document.querySelector('.nav-menu');
+ var navToggle = document.querySelector('.main-nav__toggle');
+ var closeModal = document.querySelector('.btn-close');
+ var openModal = document.querySelector('.write-us');
+
+
+openModal.addEventListener('click',function(){
+document.querySelector('.message-us').style.display = "block";
+});
+
+ closeModal.addEventListener('click',function(){
+ 	document.querySelector('.message-us').style.display = "none";
+ });
+
+  navMain.classList.remove('menu-nojs');
+  console.log(1);
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('nav-menu--closed')) {
+      navMain.classList.remove('nav-menu--closed');
+      navMain.classList.add('nav-menu--opened');
+    } else {
+      navMain.classList.add('nav-menu--closed');
+      navMain.classList.remove('nav-menu--opened');
+    }
+  });
